c10522d9ac0aef341b81e95858a2d54d9534217a
MaxRudim <max.hermsdorff@gmail.com>
2025-01-29 08:07:36 -0300

    Feature: resolução do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+
+var nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+
+nokia.Ligar();
+
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\nSmartphone iPhone:");
+
+var iphone = new Iphone("987654321", "iPhone 12", "987654321", 128);
+
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("Instagram");
